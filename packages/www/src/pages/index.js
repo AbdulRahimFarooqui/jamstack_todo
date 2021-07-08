e404bc0aa8ad130b5af94e144c759e4e35fa4f5d
AbdulRahimFarooqui <abdurraheemfarooqui@gmail.com>
2021-07-08 11:13:38 +0500

    netlify identity added

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Container, Button, Flex, Heading } from 'theme-ui'
+import netlifyID from 'netlify-identity-widget'
 
-export default props => (
-    <div>
-        Hello world
-    </div>
-)
+export default props => {
+
+    useEffect(() => {
+        netlifyID.init({})
+    }, [])
+
+    return (
+        <Container>
+            <Flex sx={{ flexDirection: "column", padding: 5 }}>
+                <Heading as="h1">Todo App</Heading>
+                <Button sx={{ marginTop: 3 }}
+                    onClick={() => { netlifyID.open() }}
+                >Login</Button>
+            </Flex>
+        </Container>
+    )
+}
